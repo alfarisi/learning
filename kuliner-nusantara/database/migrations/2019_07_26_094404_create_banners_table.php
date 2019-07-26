27393ff95b3199402d26a550495e52d1fb6a841a
al farisi <farisi@linux.alfarisi.web.id>
2019-07-26 18:30:25 +0700

    migration and auth

---
+++ kuliner-nusantara/database/migrations/2019_07_26_094404_create_banners_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('imageurl');
+            $table->string('clickurl');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
---
+++ kuliner-nusantara/database/migrations/2019_07_26_094436_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedInteger('category_id')->references('id')->on('categories')->onUpdate('cascade');
+            $table->unsignedTinyInteger('rating')->default(0);
+            $table->unsignedDecimal('price', 10, 0);
+            $table->unsignedInteger('qty_stock')->default(0);
+            $table->unsignedInteger('qty_sold')->default(0);
+            $table->boolean('is_halal')->default(1);
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
